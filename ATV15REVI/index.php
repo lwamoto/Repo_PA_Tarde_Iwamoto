5eccc0639035a2c7953f20c54aeb1e4d0592a469
lwamoto <131209284+lwamoto@users.noreply.github.com>
2026-01-30 17:00:49 -0300

    Atv15 - php
    revisao de html e css

---
+++ ATV15REVI/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ATV 15 REVI</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="web">
+        <div class="linha1">
+            <div class="reblue">
+                <div class="sqyellow"></div>
+            </div>
+            <div class="reyellow">
+                <div class="sqblue"></div>
+
+            </div>
+        </div>
+        <div class="linha2">
+            <div class="rewhite">
+                <div class="sqyellow2">
+
+                </div>
+                <div class="sqblue2">
+
+                </div>
+            </div>
+        </div>
+        <div class="linha3">
+
+            <div class="reblue3">
+                <div class="sqyellow3"></div>
+                <div class="sqyellow3"></div>
+                <div class="sqyellow3"></div>
+                <div class="sqyellow3"></div>
+                <div class="sqyellow3"></div>
+            </div>
+
+        </div>
+
+
+
+
+
+    </div>
+</body>
+</html>
